d7644a79a4f2194abcd1e1faca63cd7e17df432c
Chase Crowder <gcrowder93@gmail.com>
2021-12-15 17:49:04 -0800

    finished product. links on books work, and added more buttons on details list

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './views/Home/Home';
 import Book from './components/book/Book';
+import BookDetail from './views/Books/BookDetail';
 
 function App() {
   return (
     <main className="container">
       <BrowserRouter>
+        <h1>Library Catalog</h1>
         <Switch>
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route path="/books/:id">
+            <BookDetail />
           </Route>
           <Route path="/books">
             <BookList />
--- src/views/Books/BookDetail.js
+++ src/views/Books/BookDetail.js
@@ -1,18 +1,31 @@
 import { useEffect, useState } from 'react';
 import Book from '../../components/book/Book';
 import { getBookById } from '../../services/books';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import '../../App.css';
 
 function BookDetail() {
-  const id = 1; // TODO: Use id from route
+  const { id } = useParams(); // TODO: Use id from route
   const [book, setBook] = useState(null);
 
   useEffect(() => {
     getBookById(id).then(({ data }) => setBook(data));
   }, [id]);
-
   if (!book) return <h3>Loading book...</h3>;
 
-  return <Book book={book} showDetail />;
+  return (
+    <>
+      <Book book={book} showDetail />
+      <Link className="detailhome" to={`/`}>
+        Back To Home
+      </Link>
+      <br></br>
+      <Link className="detailbook" to={`/books`}>
+        Back To Book CataLog
+      </Link>
+    </>
+  );
 }
 
 export default BookDetail;
